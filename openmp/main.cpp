2e197203bf52679d13a0b396eea14e6d59e392d0
yuan <yuan.luo@tum.de>
2023-05-29 20:40:12 +0200

    remove validation in openmp

--- openmp/main.cpp
+++ openmp/main.cpp
@@ -5,18 +5,6 @@
 #include <ctime>
 #include <iostream>
 
-void sequentialMatrixMultiplication(double** matrix1, double** matrix2, double** result, unsigned long M, unsigned long N) {
-    for (int i = 0; i < M; ++i) {
-        for (int j = 0; j < M; ++j) {
-            double temp = 0.0;
-            for (int k = 0; k < N; ++k) {
-                temp += matrix1[i][k] * matrix2[k][j];
-            }
-            result[i][j] = temp;
-        }
-    }
-}
-
 int main(int argc, char const* argv[]) {
     if (argc < 3) {
         printf("./main.openmp (M=1250) (N=1250)\n");
@@ -41,11 +29,6 @@ int main(int argc, char const* argv[]) {
         matrix2[i] = new double[N];
     }
 
-    double** matrix3 = new double*[N];
-    for (int i = 0; i < N; i++) {
-        matrix3[i] = new double[M];
-    }
-
     double** result = new double*[M];
     for (int i = 0; i < M; i++) {
         result[i] = new double[M];
@@ -59,7 +42,6 @@ int main(int argc, char const* argv[]) {
     for (int i = 0; i < M; i++) {
         for (int j = 0; j < N; j++) {
             matrix2[i][j] = static_cast<double>(rand()) / RAND_MAX;
-            matrix3[j][i] = matrix2[i][j];
         }
     }
     auto kernel_start = std::chrono::high_resolution_clock::now();
@@ -80,28 +62,6 @@ int main(int argc, char const* argv[]) {
     double flops = flops_count / (duration.count() * 1e-6);
     std::cout << "Kernel: " << (duration.count() * 1e-6) << "s" << std::endl;
     std::cout << "FLOPS: " << flops << std::endl;
-
-    double** sequentialResult = new double*[M];
-    for (int i = 0; i < M; i++) {
-        sequentialResult[i] = new double[M];
-    }
-
-    sequentialMatrixMultiplication(matrix1, matrix3, sequentialResult, M, N);
-
-    bool isValid = true;
-    for (int i = 0; i < M; i++) {
-        for (int j = 0; j < M; j++) {
-            if (result[i][j] != sequentialResult[i][j]) {
-                isValid = false;
-                break;
-            }
-        }
-        if (!isValid) {
-            break;
-        }
-    }
-
-    std::cout << "Validation: " << (isValid ? "Passed" : "Failed") << std::endl;
 
     for (int i = 0; i < M; i++) {
         delete[] matrix1[i];
